944b7bb7ec17884375cf65563aa83aea91f0a9f0
이원재 <136036465+lepe99@users.noreply.github.com>
2026-06-12 15:45:12 +0900

    [Passed] 따옴표 출력 (89ms, 9MB)

---
+++ trail0/따옴표 출력/print-quote.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        String a = "He says \"It\'s a really simple sentence\".";
+        System.out.print(a);
+    }
+}
