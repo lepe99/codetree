3946e26ab5675550c71532fbed825aed004c1e7e
이원재 <136036465+lepe99@users.noreply.github.com>
2026-06-12 19:35:06 +0900

    [Passed] A 부터 B 까지 (139ms, 10MB)

---
+++ trail0/A 부터 B 까지/a-to-b.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        while (true) {
+            System.out.print(A + " ");
+            if (A % 2 == 0) A += 3;
+            else A *= 2;
+            if (A > B) break;
+        }       
+    }
+}
