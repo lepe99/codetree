2523941329e6e69e4153c6e9087eb506038c153c
이원재 <136036465+lepe99@users.noreply.github.com>
2026-06-12 12:26:36 +0900

    [Passed] N번 더하기 (124ms, 10MB)

---
+++ trail0/N번 더하기/add-n-times.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int N = sc.nextInt();
+        int sum = A;
+        for (int i = 0; i < N; i++) {
+            sum += N;
+            System.out.println(sum);
+        }
+    }
+}
