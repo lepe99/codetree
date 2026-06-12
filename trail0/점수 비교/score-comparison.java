b904f56d8e84a26e293399cb31c7c476a61104fc
이원재 <136036465+lepe99@users.noreply.github.com>
2026-06-12 12:43:34 +0900

    [Passed] 점수 비교 (130ms, 10MB)

---
+++ trail0/점수 비교/score-comparison.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int A1 = sc.nextInt();
+        int A2 = sc.nextInt();
+        int B1 = sc.nextInt();
+        int B2 = sc.nextInt();
+
+        if (A1 > B1 && A2 > B2) System.out.print(1);
+        else System.out.print(0);
+
+    }
+}
