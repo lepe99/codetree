b5e9534eb0e833c747cf4ed13f00caaca2946c8e
이원재 <136036465+lepe99@users.noreply.github.com>
2026-06-13 14:42:31 +0900

    [Passed] 합을 복사하기 (91ms, 9MB)

---
+++ trail1/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int sum = a + b + c;
+        a = sum;
+        b = sum;
+        c = sum;
+        System.out.print(a + " " + b + " " + c);
+    }
+}
