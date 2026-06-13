a83fe55cac5f8939e346bca3a119402d04006c27
이원재 <136036465+lepe99@users.noreply.github.com>
2026-06-13 13:26:49 +0900

    [Passed] 다양하게 출력 (88ms, 9MB)

---
+++ trail1/다양하게 출력/print-in-variety.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        String s = "Total days in Year\n365\nCircumference rate\n3.1415926535";
+
+        System.out.print(s);
+    }
+}
