a293495ae630ba4293afe7a9f7d5b46322f68293
이원재 <136036465+lepe99@users.noreply.github.com>
2026-06-13 13:23:18 +0900

    [Passed] 한줄 출력 (89ms, 9MB)

---
+++ trail1/한줄 출력/print-one-line.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        String s = "Let\'s go LeebrosCode!";
+        System.out.print(s);
+    }
+}
