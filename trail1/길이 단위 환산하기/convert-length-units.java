ab09e120219cb1af0a542d1be2cd3599485e1f00
이원재 <136036465+lepe99@users.noreply.github.com>
2026-06-13 14:47:19 +0900

    [Passed] 길이 단위 환산하기 (150ms, 12MB)

---
+++ trail1/길이 단위 환산하기/convert-length-units.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%.1f", sc.nextDouble() * 30.48);
+    }
+}
