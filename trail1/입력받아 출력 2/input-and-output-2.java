bf9a44369c6fb92e77f5d06f231590bd2f490d24
이원재 <136036465+lepe99@users.noreply.github.com>
2026-06-13 15:09:25 +0900

    [Passed] 입력받아 출력 2 (121ms, 10MB)

---
+++ trail1/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(sc.next(), "-");
+        System.out.print(st.nextToken() + st.nextToken());
+    }
+}
