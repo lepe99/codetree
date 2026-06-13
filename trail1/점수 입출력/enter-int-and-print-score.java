abd0e17db030a5202daedfcc839313ac86bd049f
이원재 <136036465+lepe99@users.noreply.github.com>
2026-06-13 14:45:17 +0900

    [Passed] 점수 입출력 (128ms, 10MB)

---
+++ trail1/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Your score is " + sc.nextInt() + " point.");
+    }
+}
