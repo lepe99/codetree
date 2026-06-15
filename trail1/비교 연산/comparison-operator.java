dbe5e9d1c2af08c96f1ce704e17d8d594d5f0529
이원재 <136036465+lepe99@users.noreply.github.com>
2026-06-15 15:13:44 +0900

    [Passed] 비교 연산 (133ms, 11MB)

---
+++ trail1/비교 연산/comparison-operator.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        if (A >= B) System.out.println(1);
+        else System.out.println(0);
+        if (A > B) System.out.println(1);
+        else System.out.println(0);
+        if (A <= B) System.out.println(1);
+        else System.out.println(0);
+        if (A < B) System.out.println(1);
+        else System.out.println(0);
+        if (A == B) System.out.println(1);
+        else System.out.println(0);
+        if (A != B) System.out.println(1);
+        else System.out.println(0);
+        
+    }
+}
