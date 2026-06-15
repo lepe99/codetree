d64a634dacae14ef369e5dc6daf83af4dd608747
이원재 <136036465+lepe99@users.noreply.github.com>
2026-06-15 14:50:45 +0900

    [Passed] 살 수 있는 물건 (148ms, 10MB)

---
+++ trail1/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n >= 3000) System.out.print("book");
+        else if (n >= 1000) System.out.print("mask");
+        else System.out.print("no");
+    }
+}
