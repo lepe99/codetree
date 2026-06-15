85ff1dd2972fcb18232491c4f3343f8aefe8e0cb
이원재 <136036465+lepe99@users.noreply.github.com>
2026-06-15 21:53:25 +0900

    [Passed] 일 수 구하기 (133ms, 11MB)

---
+++ trail1/일 수 구하기/number-of-days-in-month.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        System.out.print(arr[n]);
+    }
+}
