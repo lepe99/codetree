8e9a7c75a9b39418bb7a91c2a6effa41ff59544a
이원재 <136036465+lepe99@users.noreply.github.com>
2026-06-16 14:48:13 +0900

    [Passed] 홀수만 출력하기 (130ms, 10MB)

---
+++ trail1/홀수만 출력하기/print-only-odd-numbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a;
+        for (int i = 0; i < n; i++) {
+            a = sc.nextInt();
+            if (a % 2 == 1 && a % 3 == 0) {
+                System.out.println(a);
+            }
+        }
+    }
+}
