5baa1eb0acddc7cb1c9f96d05e669cb44fb3d1e3
이원재 <136036465+lepe99@users.noreply.github.com>
2026-06-17 15:50:34 +0900

    [Passed] 등장횟수 확인 (129ms, 10MB)

---
+++ trail1/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+        for (int i = 0; i < 5; i++) {
+            if (sc.nextInt() % 2 == 0) cnt++;
+        }
+        System.out.print(cnt);
+    }
+}
