85d62c5558ae7db6dbbdb2d7f7f9cda38954d521
이원재 <136036465+lepe99@users.noreply.github.com>
2026-07-14 13:54:58 +0900

    [Passed] 별표 출력하기 8 (2ms, 0MB)

---
+++ trail1/별표 출력하기 8/print-star-8.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 1) {
+            for (int j = 0; j < i + 1; j++) {
+                cout << "* ";
+            }
+        } else {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
