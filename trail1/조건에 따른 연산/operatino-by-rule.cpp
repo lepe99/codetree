59b5f73627b6db7f3418ce481fd9f86b2ce03cc5
이원재 <136036465+lepe99@users.noreply.github.com>
2026-07-14 10:29:12 +0900

    [Passed] 조건에 따른 연산 (2ms, 0MB)

---
+++ trail1/조건에 따른 연산/operatino-by-rule.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, cnt = 0;
+    cin >> n;
+    while (true) {
+        if (n % 2 == 0) n = n * 3 + 1;
+        else n = n * 2 + 2;
+        cnt++;
+        if (n >= 1000) break;
+    }
+    cout << cnt;
+    return 0;
+}
