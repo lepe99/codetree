3c0102c6a6c1192519085440cdf444a38acc6da7
이원재 <136036465+lepe99@users.noreply.github.com>
2026-07-16 11:07:54 +0900

    [Passed] 숫자 피라미드 2 (2ms, 0MB)

---
+++ trail1/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, cnt = 0;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cnt++;
+            cout << cnt << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
