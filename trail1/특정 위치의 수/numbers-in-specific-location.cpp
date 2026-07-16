9f092dede1e64b98f26ccff2d49be1b89f987e7b
이원재 <136036465+lepe99@users.noreply.github.com>
2026-07-16 17:20:48 +0900

    [Passed] 특정 위치의 수 (2ms, 0MB)

---
+++ trail1/특정 위치의 수/numbers-in-specific-location.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a[10], n;
+    for (int i = 0; i < 10; i++) {
+        cin >> a[i];
+    }
+    cout << a[2] + a[4] + a[9];
+    return 0;
+}
