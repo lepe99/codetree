bbae90b80a1b377958011e506b899c14c9383d87
이원재 <136036465+lepe99@users.noreply.github.com>
2026-07-16 09:27:14 +0900

    [Passed] 정사각형 출력 (2ms, 0MB)

---
+++ trail1/정사각형 출력/print-square.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, cnt = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cnt++;
+            cout << cnt << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
