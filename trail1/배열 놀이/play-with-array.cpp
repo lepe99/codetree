b6a5edcc42bf2af64f00bdea88d534cf947afdf7
이원재 <136036465+lepe99@users.noreply.github.com>
2026-07-17 21:53:24 +0900

    [Passed] 배열 놀이 (2ms, 0MB)

---
+++ trail1/배열 놀이/play-with-array.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, q;
+    cin >> n >> q;
+    int* a = new int[n];
+    for (int i = 0; i < n; i++) {
+        int inp;
+        cin >> inp;
+        a[i] = inp;
+    }
+    for (int i = 0; i < q; i++) {
+        int f, s, t;
+        cin >> f >> s;
+        if (f == 1) cout << a[s - 1];
+        else if (f == 2) {
+            bool f = 0;
+            for (int i = 0; i < n; i++) {
+                if (a[i] == s) {
+                    f = 1;
+                    cout << i + 1;
+                    break;
+                }
+            }
+            if (!f) cout << 0;
+        }
+        else {
+            cin >> t;
+            for (int i = s - 1; i < t; i++) {
+                cout << a[i] << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
