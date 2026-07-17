a658de1a7f2365ef12be3cf52a1e5f6096c444fb
이원재 <136036465+lepe99@users.noreply.github.com>
2026-07-17 20:47:57 +0900

    [Passed] 나눗셈의 나머지 (2ms, 0MB)

---
+++ trail1/나눗셈의 나머지/remainder-of-division.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    int* arr = new int[b]{};
+    while (a > 1) {
+        ++arr[a % b];
+        a /= b;
+    }
+    int sum = 0;
+    for (int i = 0; i < b; i++) {
+        sum += arr[i] * arr[i];
+    }
+    cout << sum;
+    return 0;
+}
