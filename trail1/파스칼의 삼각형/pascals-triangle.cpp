21a124a7bc2d988ef18e6b115834d0b690e44862
이원재 <136036465+lepe99@users.noreply.github.com>
2026-07-23 16:02:33 +0900

    [Passed] 파스칼의 삼각형 (2ms, 0MB)

---
+++ trail1/파스칼의 삼각형/pascals-triangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[15][15] {};
+    for (int i = 0; i < n; i++) {
+        arr[i][0] = 1;
+    }
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < n; j++) {
+            arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i + 1; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
