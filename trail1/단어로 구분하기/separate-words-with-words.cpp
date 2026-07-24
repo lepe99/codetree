294aef2c2bdd96fd1620cc0a7689dfefbcee397f
이원재 <136036465+lepe99@users.noreply.github.com>
2026-07-24 11:17:35 +0900

    [Passed] 단어로 구분하기 (2ms, 0MB)

---
+++ trail1/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#define M 201
+using namespace std;
+
+int main() {
+    char s[M];
+    for (int i = 0; i < 10; i++) {
+        cin >> s;
+        cout << s << endl;
+    }
+    return 0;
+}
