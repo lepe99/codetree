74b5ade07c286a7e5d533b5f4b8a54e44a145466
이원재 <136036465+lepe99@users.noreply.github.com>
2026-07-28 14:44:43 +0900

    [Passed] 붙여서 합하기 (2ms, 0MB)

---
+++ trail1/붙여서 합하기/add-and-add.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int stoi(char s[]) {
+    int res{};
+    res = s[0] - '0';
+    for (int i = 1; s[i]; i++) {
+        res = res * 10 + (s[i] - '0');
+    }
+    return res;
+}
+
+int main() {
+    // Please write your code here.
+    char a[5], b[5], ab[9], ba[9];
+    cin >> a >> b;
+    int len_ab{-1}, len_ba{-1};
+    for (int i = 0; a[i]; i++) ab[++len_ab] = a[i]; 
+    for (int i = 0; b[i]; i++) {
+        ab[++len_ab] = b[i]; 
+        ba[++len_ba] = b[i]; 
+    }
+    for (int i = 0; a[i]; i++) ba[++len_ba] = a[i]; 
+    ab[++len_ab] = '\0'; 
+    ba[++len_ba] = '\0';
+    cout << stoi(ab) + stoi(ba);
+    return 0;
+}
