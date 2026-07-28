f9167328c2835f7ee0a2973097caa66ecc628e30
이원재 <136036465+lepe99@users.noreply.github.com>
2026-07-28 13:30:46 +0900

    [Passed] 아스키코드표 맞추기 (2ms, 0MB)

---
+++ trail1/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    for (int i = 0; i < 5; i++) {
+        int a;
+        cin >> a;
+        cout << (char) a << " ";
+    }
+    return 0;
+}
