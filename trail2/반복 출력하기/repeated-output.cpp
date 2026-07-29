15fe600fa98e3c09509b3387dbc4d118e9fa9139
이원재 <136036465+lepe99@users.noreply.github.com>
2026-07-29 16:38:57 +0900

    [Passed] 반복 출력하기 (1ms, 0MB)

---
+++ trail2/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void print_s(int n) {
+    for (int i = 0; i < n; i++) 
+        cout << "12345^&*()_" << endl;
+}
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    print_s(n);
+    return 0;
+}
