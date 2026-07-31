a89fb71dafb0cd436835beb186443103dc1ac8b9
이원재 <136036465+lepe99@users.noreply.github.com>
2026-07-31 11:37:55 +0900

    [Passed] Factorial (2ms, 0MB)

---
+++ trail2/Factorial/factorial.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int sol(int n) {
+    if (n == 1) return 1;
+    return sol(n - 1) * n;
+}
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    cout << sol(n);
+    return 0;
+}
