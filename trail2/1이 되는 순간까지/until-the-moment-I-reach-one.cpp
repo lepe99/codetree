1637f2424d077d6e0e4d557b9ff8e30d4354b020
이원재 <136036465+lepe99@users.noreply.github.com>
2026-07-31 10:03:22 +0900

    [Passed] 1이 되는 순간까지 (2ms, 0MB)

---
+++ trail2/1이 되는 순간까지/until-the-moment-I-reach-one.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int sol(int n) {
+    if (n == 1) return 0;
+    if (n % 2 == 0) return sol(n / 2) + 1;
+    else return sol(n / 3) + 1;
+}
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    cout << sol(n);
+    return 0;
+}
