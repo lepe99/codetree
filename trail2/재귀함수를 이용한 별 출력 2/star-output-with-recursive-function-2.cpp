65112d4e920d37601ce98a0a2f5a4e56929c2a4d
이원재 <136036465+lepe99@users.noreply.github.com>
2026-07-31 09:08:57 +0900

    [Passed] 재귀함수를 이용한 별 출력 2 (2ms, 0MB)

---
+++ trail2/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void sol(int n) {
+    if (n == 0) return;
+    for (int i = 0; i < n; i++) {
+        cout << "* ";
+    }
+    cout << endl;
+    sol(n - 1);
+    for (int i = 0; i < n; i++) {
+        cout << "* ";
+    }
+    cout << endl;
+}
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    sol(n);
+    return 0;
+}
