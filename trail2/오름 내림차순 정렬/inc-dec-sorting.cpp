f19e1c6673b858b6429d02936fc158ecd1dd1300
이원재 <136036465+lepe99@users.noreply.github.com>
2026-08-10 14:09:34 +0900

    [Passed] 오름 내림차순 정렬 (2ms, 0MB)

--- trail2/오름 내림차순 정렬/inc-dec-sorting.cpp
+++ trail2/오름 내림차순 정렬/inc-dec-sorting.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#define MAX_N 101
+
 using namespace std;
+
+int a[MAX_N];
+int temp[MAX_N];
+
+
 // 병합정렬 구현 연습
-void mergeRange(int* a, int* temp, int left, int mid, int right) {
+void merge(int left, int mid, int right) {
     int i = left;
     int j = mid + 1;
     int k = left;
@@ -11,41 +18,29 @@ void mergeRange(int* a, int* temp, int left, int mid, int right) {
         else temp[k++] = a[j++];
     }
 
-    while(i <= mid) temp[k++] =a [i++];
-    while(j <= right) temp[k++] =a [j++];
+    while(i <= mid) temp[k++] = a[i++];
+    while(j <= right) temp[k++] = a[j++];
 
     for (int i = left; i <= right; i++) a[i] = temp[i];
 }
 
-void mergeSortRec(int* a, int* temp, int left, int right) {
+void mergeSort(int left, int right) {
     if (left >= right) return;
     int mid = left + (right - left) / 2;
-    mergeSortRec(a, temp, left, mid);
-    mergeSortRec(a, temp, mid + 1, right);
-    mergeRange(a, temp, left, mid, right);
-}
-
-void mergeSort(int* a, int n) {
-    if (n <= 1) return;
-    int* temp = new int[n];
-    mergeSortRec(a, temp, 0, n - 1);
-    delete[] temp;
+    mergeSort(left, mid);
+    mergeSort(mid + 1, right);
+    merge(left, mid, right);
 }
 
 int main() {
     int n;
     cin >> n;
-    int* a = new int[n];
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-
-    mergeSort(a, n);
+    mergeSort(0, n - 1);
     for (int i = 0; i < n; i++) cout << a[i] << " ";
     cout << endl;
     for (int i = n - 1; i >= 0; i--) cout << a[i] << " ";
-    
-
-    delete[] a;
     return 0;
 }
